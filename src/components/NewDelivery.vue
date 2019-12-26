4dba81ea5c77092233f9ba332ac94afdd7ec09fc
MrByerikbol <berikboltt@gmail.com>
2019-12-27 02:39:16 +0600

    order form deer shinchilelt hiilee

--- src/components/NewDelivery.vue
+++ src/components/NewDelivery.vue
@@ -19,7 +19,8 @@
                                     <select
                                         
                                         v-model="deliveryObject.userId"
-                                        
+                                        @change="checkLoan(deliveryObject.userId,
+                                            totalPriceOfOrder)"
                                         class="form-control"
                                     >
                                         <option value=0>--- Захиалага өгсөн клиент сонгох ---</option>
@@ -225,11 +226,55 @@
                                  <b-col lg="1" class="mt-3 pt-3 text-right">
                                      <button type="button" @click="removePvh(index,r)" class="btn btn-sm btn-warning">пвх -</button>
                                  </b-col>
+                                 
                             </b-form-row>
                         </b-col>
                     </b-form-row>
-                    <b-button type="submit" v-if="choosenProducts.length>0 && deliveryObject.userId>0" variant="primary" class="mr-2">Листийн захиалага үүсгэх</b-button>
-                    <b-button type="reset" variant="danger">Арилгах</b-button>
+                    <b-form-row class="py-3 " v-if="totalPriceOfOrder>0 && choosenProducts.length>0 && deliveryObject.userId>0">
+                        <b-col lg="2">
+                            <label class="label-container text-danger">Зээлээр эсэх
+                                <input @change="checkLoan(deliveryObject.userId,
+                                    totalPriceOfOrder)" type="checkbox" v-model="deliveryObject.isLoan">
+                                <span class="checkmark"></span>
+                            </label>                         
+                        </b-col>
+                        
+                         <b-col lg="10" class="text-right text-info font-weight-bold" style="text-decoration:underline !important;">
+                            Захиалгын нийлбэр дүн : {{totalPriceOfOrder}}
+                        </b-col>
+                        <b-col v-if="!loanResult" lg="12">
+                            <b-form-row>
+                                <b-col lg="3" class="pl-1 pb-2">
+                                    Зээлийн хэжмээ: <strong>{{loanDbResult.loanLimit}}</strong>
+                                </b-col>
+                                <b-col lg="3" class="pl-2">
+                                    Одоогийн зээл: <strong>{{loanDbResult.loanBalance}}</strong>
+                                </b-col>
+                                <b-col lg="3" class="pl-2">
+                                    Боломжит зээл: <strong>{{loanDbResult.possibleLoan}}</strong>
+                                </b-col>
+                                <b-col lg="3" class="text-right text-danger font-italic" 
+                                    style="text-decoration:underline !important;">
+                                    Зээлийн хэтрэлт: <strong>{{totalPriceOfOrder-loanDbResult.possibleLoan}}</strong>
+                                </b-col>
+                            </b-form-row>
+                        </b-col>
+                        <b-col lg="12" v-if="!loanResult && loanDbResult.loanBalance>0" class="text-right">
+                            <button type="button" class="btn btn-sm btn-secondary">
+                                зээл төлөх
+                            </button>
+                        </b-col>
+                    </b-form-row>
+                    <b-form-row>
+                        <b-col lg="12" class="text-right">
+                            <b-button type="submit" 
+                                v-if="totalPriceOfOrder>0 && choosenProducts.length>0
+                                    && deliveryObject.userId>0 && mainOrderFormChecker && loanResult" 
+                                variant="primary" class="mr-2">Листийн захиалага үүсгэх</b-button>
+                            <b-button type="reset" variant="danger">Арилгах</b-button>
+                        </b-col>
+                    </b-form-row>
+
                 </b-form>
                 
             </b-card>
@@ -246,10 +291,47 @@
 
          data() {
             return {
-                choosenProducts:[]
+                choosenProducts:[],
+                loanResult : true,
+                loanDbResult : {}
             }
         },
         methods: {
+            checkLoan(userId,loan){
+                try {
+                    if(this.deliveryFormObject.isLoan){
+                        axios.post(apiDomain+'/admin/order/checkloan',{
+                        'userId':userId,
+                        'userLoan':loan
+                        },{headers:getHeader()})
+                        .then(response=>{
+                            //alert(JSON.stringify(response.data));   
+                            if(!response.data.result){
+                                this.loanResult=false;
+                                this.loanDbResult=response.data;
+                            }
+                            else{
+                                this.loanDbResult={};
+                                this.loanResult=true;
+                            }
+                        })
+                        .catch(error => {
+                            //console.log(error.message)
+                            this.$bvToast.toast(error.message, {
+                                title: 'алдаа',
+                                autoHideDelay: 5000
+                            });
+                            this.loanResult=false;
+                        }) 
+                    }
+                    else{
+                        this.loanResult=true;
+                    }
+                }
+                catch(err){
+                    alert(err.message);
+                }  
+            },
             getTotalPvh(){
                 let totalPvh =0;
                 for(let j = 0;j<this.choosenProducts.length;j++){
@@ -259,6 +341,7 @@
                 }
                 return totalPvh;
             },
+            
             removePvh(index,r){
                 let delIndex = parseInt(r);
                 this.choosenProducts[index].relDetails.splice(delIndex,1);
@@ -267,9 +350,15 @@
                     this.choosenProducts[index].hasRelDetail=false;
 
                 }
+                if(this.deliveryFormObject.isLoan){
+                    this.checkLoan(this.deliveryFormObject.userId,this.totalPriceOfOrder);
+                }
             },
             pvhTotalCalculation(c){
                 c.totalPrice=c.productCount*c.productPrice;
+                if(this.deliveryFormObject.isLoan){
+                    this.checkLoan(this.deliveryFormObject.userId,this.totalPriceOfOrder);
+                }
             },
             pvhChange(item){
                
@@ -285,6 +374,9 @@
                     item.productPrice=found.productPrice;
                     item.productCount=1;
                     item.totalPrice=found.productPrice*1;
+                }
+                if(this.deliveryFormObject.isLoan){
+                    this.checkLoan(this.deliveryFormObject.userId,this.totalPriceOfOrder);
                 }
             },
             hasRelDetail(index){
@@ -308,6 +400,9 @@
                 else{
                      this.choosenProducts[index].relDetails=[];
                 }
+                if(this.deliveryFormObject.isLoan){
+                    this.checkLoan(this.deliveryFormObject.userId,this.totalPriceOfOrder);
+                }
             },
             addPvh(index){
                  this.choosenProducts[index].relDetails.push(
@@ -323,6 +418,14 @@
             },
             removeProduct(index){
                 this.choosenProducts.splice(index,1);
+                if(this.choosenProducts.length==0){
+                    this.deliveryFormObject.isLoan=false;
+                    this.loanResult=true;
+                    this.loanDbResult={};
+                }
+                if(this.deliveryFormObject.isLoan){
+                    this.checkLoan(this.deliveryFormObject.userId,this.totalPriceOfOrder);
+                }
             },
             productChange(item){
                 let productId=this.choosenProducts[item].deliveryProductId;
@@ -338,6 +441,9 @@
                     this.choosenProducts[item].productPrice=found.productPrice;
                     this.choosenProducts[item].productCount=1;
                     this.choosenProducts[item].totalPrice=found.productPrice*1;
+                }
+                if(this.deliveryFormObject.isLoan){
+                    this.checkLoan(this.deliveryFormObject.userId,this.totalPriceOfOrder);
                 }
             },
             addProductField(){
@@ -417,12 +523,16 @@
             totalCalculation(index){
                 this.choosenProducts[index].totalPrice=
                     this.choosenProducts[index].productPrice*this.choosenProducts[index].productCount;
+                    if(this.deliveryFormObject.isLoan){
+                        this.checkLoan(this.deliveryFormObject.userId,this.totalPriceOfOrder);
+                    }
             },
 
             onReset(evt) {
                 evt.preventDefault()
                
             }
+            
         },
         computed: {
             ...mapState([
@@ -444,10 +554,120 @@
                     return this.deliveryFormObject 
                 }        
             },
+            totalPriceOfOrder : function(){
+                let totalPrice = 0;
+                for (let m =0 ;m<this.choosenProducts.length;m++){
+                    totalPrice=totalPrice+this.choosenProducts[m].totalPrice;
+                    let detailProducts=this.choosenProducts[m].relDetails;
+                    for(let k = 0;k<detailProducts.length;k++){
+                        totalPrice=totalPrice+detailProducts[k].totalPrice;
+                    }
+                }
+                
+                return totalPrice;
+            },
             lastBalance:function(){
                 return (wareHouseId,productId) => this.getWareHouseProductBalance(wareHouseId,productId);
+            },
+            mainOrderFormChecker : function(){
+                let theResult = true;
+                try {
+                    for (let m =0 ;m<this.choosenProducts.length;m++){
+                        let list = this.choosenProducts[m];
+                        if(!list.deliveryProductId || parseInt(list.deliveryProductId)==0 || 
+                            !list.totalPrice || parseInt(list.totalPrice)==0 
+                            || !list.wareHouseId || parseInt(list.wareHouseId)==0 ){
+                                return false;
+                        }
+                        let relDetails = list.relDetails;
+                        for (let i = 0;i<relDetails.length;i++){
+                            let detailList = relDetails[i];
+                            if(!detailList.productId || parseInt(detailList.productId)==0 || 
+                                !detailList.totalPrice || parseInt(detailList.totalPrice)==0 
+                                || !detailList.wareHouseId || parseInt(detailList.wareHouseId)==0 ){
+                                    return false;
+                            }
+                        }
+                    }    
+                    return theResult;
+                }
+                catch(err){
+                    alert("form shalgaltiin aldaa ", err.message);
+                    return false;
+                }
+                
             }
+           
         }
 
     }
 </script>
+<style scoped>
+    /* The container */
+    .label-container {
+        display: block;
+        position: relative;
+        padding-left: 35px;
+        margin-bottom: 12px;
+        cursor: pointer;
+        font-size: 15px;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    /* Hide the browser's default checkbox */
+    .label-container input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+        height: 0;
+        width: 0;
+    }
+
+    /* Create a custom checkbox */
+    .checkmark {
+        position: absolute;
+        top: 0px;
+        left: 0;
+        height: 25px;
+        width: 25px;
+        background-color: #eee;
+    }
+
+    /* On mouse-over, add a grey background color */
+    .label-container:hover input ~ .checkmark {
+         background-color: #ccc;
+    }
+
+    /* When the checkbox is checked, add a blue background */
+    .label-container input:checked ~ .checkmark {
+        background-color: #dc3545;
+    }
+
+    /* Create the checkmark/indicator (hidden when not checked) */
+    .checkmark:after {
+        content: "";
+        position: absolute;
+        display: none;
+    }
+
+    /* Show the checkmark when checked */
+    .label-container input:checked ~ .checkmark:after {
+        display: block;
+    }
+
+    /* Style the checkmark/indicator */
+    .label-container .checkmark:after {
+        left: 10px;
+        top: 6px;
+        width: 5px;
+        height: 10px;
+        border: solid white;
+        border-width: 0 3px 3px 0;
+        -webkit-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        transform: rotate(45deg);
+    }
+</style>
